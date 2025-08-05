5aebe265d9af2d88d04774c8e776d1e7f5e1a34f
Roberto <robworkhub@gmail.com>
2025-08-05 17:42:04 +0200

    Fixed getit error

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4166266424"
+    serviceWorkerVersion: "3304076060"
   }
 });
