8013de1a357ce77e44e4afa339c0508206c628a2
Roberto <robworkhub@gmail.com>
2025-08-08 15:57:53 +0200

    Layout in Order Page FIXED OVERFLOW

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3173185322"
+    serviceWorkerVersion: "3273881353"
   }
 });
